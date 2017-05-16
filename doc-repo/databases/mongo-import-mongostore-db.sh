51f4eaa616a55062ab2c06626dfc6549cc548ab5
Paulo Sérgio da Silva <pss1suporte@gmail.com>
2017-05-16 18:54:39 -0300

    Script: mongo-import-mongostore-db.sh

--- doc-repo/databases/mongo-import-mongostore-db.sh
+++ doc-repo/databases/mongo-import-mongostore-db.sh
@@ -12,24 +12,28 @@ echo "############################"
 echo "Clonando o Projeto"
 echo "############################"
 git clone https://github.com/tmcnab/northwind-mongo.git 
-cd northwind-mongo
-echo ""
+
+cd northwind-mongo/
+PATH_TO=$(pwd)
+
+echo "PATH_TO=$PATH_TO" 
 echo "############################"
 echo "Importanto as coleções!"
-mongorestore --db northwind --collection categories --nsInclude dump/ categories.bson
-mongorestore --db northwind --collection customers --nsInclude dump/ customers.bson
-mongorestore --db northwind --collection employee-territories --nsInclude dump/ employee-territories.bson
-mongorestore --db northwind --collection employees --nsInclude dump/ employees.bson
-mongorestore --db northwind --collection order-details --nsInclude dump/ order-details.bson
-mongorestore --db northwind --collection orders --nsInclude dump/ orders.bson
-mongorestore --db northwind --collection products --nsInclude dump/ products.bson
-mongorestore --db northwind --collection regions --nsInclude dump/ regions.bson
-mongorestore --db northwind --collection shippers --nsInclude dump/ shippers.bson
-mongorestore --db northwind --collection suppliers --nsInclude dump/ suppliers.bson
-mongorestore --db northwind --collection system.indexes --nsInclude dump/ system.indexes.bson
-mongorestore --db northwind --collection territories --nsInclude dump/ territories.bson
+mongorestore --db northwind --collection categories $PATH_TO/dump/categories.bson
+mongorestore --db northwind --collection customers $PATH_TO/dump/customers.bson
+mongorestore --db northwind --collection employee-territories $PATH_TO/dump/employee-territories.bson
+mongorestore --db northwind --collection employees $PATH_TO/dump/employees.bson
+mongorestore --db northwind --collection order-details $PATH_TO/dump/order-details.bson
+mongorestore --db northwind --collection orders $PATH_TO/dump/orders.bson
+mongorestore --db northwind --collection products $PATH_TO/dump/products.bson
+mongorestore --db northwind --collection regions $PATH_TO/dump/regions.bson
+mongorestore --db northwind --collection shippers $PATH_TO/dump/shippers.bson
+mongorestore --db northwind --collection suppliers $PATH_TO/dump/suppliers.bson
+mongorestore --db northwind --collection system.indexes $PATH_TO/dump/system.indexes.bson
+mongorestore --db northwind --collection territories $PATH_TO/dump/territories.bson
 echo "############################"
 echo ""
 ############################
 
 # Linux Command Line and Shell Scripting Bible 3rd Edition
+
